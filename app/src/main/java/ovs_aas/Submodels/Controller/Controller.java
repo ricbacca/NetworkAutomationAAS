8042c7fb27e580b119cbd23eabd73c568b2811ad
Riccardo <riccardo.bacca@gmail.com>
2024-02-09 14:21:57 +0100

    Controller Submodel improvements

---
+++ app/src/main/java/ovs_aas/Submodels/Controller/Controller.java
@@ -0,0 +1,134 @@
+// Copyright 2023 riccardo.bacca@studio.unibo.it
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ovs_aas.Submodels.Controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.basyx.submodel.metamodel.map.Submodel;
+import org.eclipse.basyx.submodel.metamodel.map.qualifier.LangStrings;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.valuetype.ValueType;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.operation.Operation;
+
+import ovs_aas.Submodels.SubmodelImpl.AbstractSubmodel;
+import ovs_aas.Submodels.Utils.Utils;
+import ovs_aas.RyuController.ApiEnum;
+
+public class Controller extends AbstractSubmodel {
+
+    private ControllerLambda lambdaProvider;
+    private final int controllerId;
+
+    public Controller(int controllerNumber) {
+        super();
+        this.controllerId = controllerNumber;
+        this.lambdaProvider = new ControllerLambda(this.getController());
+    }
+
+    @Override
+    public List<Submodel> createSubmodel() {
+        Submodel cnt1Submodel = new Submodel();
+
+		cnt1Submodel.setIdShort("Controller" + controllerId);
+
+        cnt1Submodel.addSubmodelElement(aggregateFlowStats());
+        cnt1Submodel.addSubmodelElement(allFlowStats());
+        cnt1Submodel.addSubmodelElement(getRole());
+        cnt1Submodel.addSubmodelElement(setRole());
+        cnt1Submodel.addSubmodelElement(setFirewallRules());
+        cnt1Submodel.addSubmodelElement(getFirewallRules());
+        cnt1Submodel.addSubmodelElement(deleteFirewallRule());
+
+		return List.of(cnt1Submodel);
+    }
+
+    private Operation aggregateFlowStats() {
+        Operation aggregateFlows = new Operation("AggregateFlows");
+        aggregateFlows.setOutputVariables(getUtils().getOutputVariables(Utils.AGGREGATE_FLOWS_OUTPUT));
+        aggregateFlows.setWrappedInvokable(
+            lambdaProvider.getAggregateStats(ApiEnum.getElement(controllerId, ApiEnum.GETAGGREGATEFLOWSTATS)));
+
+        return aggregateFlows;
+    }
+
+
+    private Operation allFlowStats() {
+        Operation allFlowStats = new Operation("AllFlowStats");
+
+        allFlowStats.setOutputVariables(getUtils().getOutputVariables(Utils.ALL_FLOWS_OUTPUT));
+        allFlowStats.setWrappedInvokable(
+            lambdaProvider.getAllFlowStats(ApiEnum.getElement(controllerId, ApiEnum.GETALLFLOWSTATS)));
+
+        return allFlowStats;
+    }
+
+    private Operation getRole() {
+        Operation getRole = new Operation("GetRole");
+
+        getRole.setOutputVariables(getUtils().getOutputVariables(Utils.GET_ROLE_OUTPUT));
+        getRole.setWrappedInvokable(
+            lambdaProvider.getRole(ApiEnum.getElement(controllerId, ApiEnum.GETROLE)));
+
+        return getRole;
+    }
+
+    private Operation setRole() {
+        Operation setRole = new Operation("SetRole");
+        setRole.setInputVariables(getUtils().getCustomInputVariables(
+            Map.of("Role", ValueType.String)
+        ));
+        setRole.setOutputVariables(getUtils().getOutputVariables(Utils.SET_ROLE_OUTPUT));
+        setRole.setWrappedInvokable(
+            lambdaProvider.setRole(ApiEnum.getElement(controllerId, ApiEnum.SETROLE)));
+
+        return setRole;
+    }
+
+    private Operation setFirewallRules() {
+        Operation setRule = new Operation("SetFirewallRules");
+        setRule.setDescription(new LangStrings("English", "Source and Destination between 1 and 6. Add rules for both ways."));
+        setRule.setInputVariables(getUtils().getCustomInputVariables(Map.of(
+            "Source", ValueType.String,
+            "Destination", ValueType.String
+        )));
+        setRule.setOutputVariables(getUtils().getOutputVariables(1));
+        setRule.setWrappedInvokable(lambdaProvider.setFirewallRule(this.getFirewallUrl()));
+
+        return setRule;
+    }
+
+    private Operation getFirewallRules() {
+        Operation getRules = new Operation("GetFirewallRules");
+        getRules.setOutputVariables(getUtils().getOutputVariables(Utils.GET_FIREWALL_RULES));
+
+        getRules.setWrappedInvokable(lambdaProvider.getFirewallRules(this.getFirewallUrl()));
+
+        return getRules;
+    }
+
+    private Operation deleteFirewallRule() {
+        Operation deleteRule = new Operation("DeleteFirewallRules");
+        deleteRule.setInputVariables(getUtils().getCustomInputVariables(Map.of("RuleId", ValueType.Integer)));
+        deleteRule.setOutputVariables(getUtils().getOutputVariables(1));
+
+        deleteRule.setWrappedInvokable(lambdaProvider.deleteFirewallRules(this.getFirewallUrl()));
+
+        return deleteRule;
+    }
+
+    private String getFirewallUrl() {
+        return ApiEnum.getElement(controllerId, ApiEnum.GETFIREWALLRULES);
+    }
+}
---
+++ app/src/main/java/ovs_aas/Submodels/Controller/ControllerLambda.java
@@ -0,0 +1,99 @@
+package ovs_aas.Submodels.Controller;
+
+import java.math.BigInteger;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.EnumUtils;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.SubmodelElement;
+import org.eclipse.basyx.submodel.metamodel.map.submodelelement.dataelement.property.Property;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import ovs_aas.RyuController.Models.AggregateFlowStats;
+import ovs_aas.RyuController.Models.AllFlowStats;
+import ovs_aas.RyuController.Models.Role;
+import ovs_aas.RyuController.Models.RoleEnum;
+import ovs_aas.RyuController.Models.AccessControlList.AccessControlList;
+import ovs_aas.Submodels.Utils.RyuControllerManager;
+
+public class ControllerLambda {
+
+    private RyuControllerManager controller;
+
+    public ControllerLambda(RyuControllerManager controller) {
+        this.controller = controller;
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> getAggregateStats(String url) {
+        return (args) -> {
+            Map<String, AggregateFlowStats[]> res = controller.getResponseWithSerial(url, new TypeReference<Map<String, AggregateFlowStats[]>>(){});
+            return res.values().stream().findFirst().get()[0].formatResult();
+        };
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> getAllFlowStats(String url) {
+        return (args) -> {
+            Map<String, AllFlowStats[]> res = controller.getResponseWithSerial(
+                url, 
+                new TypeReference<Map<String, AllFlowStats[]>>(){});
+            
+            return AllFlowStats.formatMultipleResults(res.values().stream().collect(Collectors.toList()));
+        };
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> getRole(String url) {
+        return (args) -> {
+            Map<String, Role[]> res = controller.getResponseWithSerial(
+                url, 
+                new TypeReference<Map<String, Role[]>>(){});
+            
+            return res.values().stream().findFirst().get()[0].formatResult();
+        };
+    }
+
+        public Function<Map<String, SubmodelElement>, SubmodelElement[]> setRole(String url) {
+        return (args) -> {
+            int statusCode = 0;
+            String input = (String) args.get("Role").getValue();
+            if (EnumUtils.isValidEnum(RoleEnum.class, input)) {
+                statusCode = controller.setRole(url, 1, RoleEnum.valueOf(input));
+            } else statusCode = 400;
+            
+            return new SubmodelElement[] {
+                new Property("StatusCode: " + statusCode)
+            }; 
+        }; 
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> setFirewallRule(String url) {
+        return (args) -> {
+            String src = args.get("Source").getValue() == null ? "" : args.get("Source").getValue().toString();
+            String dst = args.get("Destination").getValue() == null ? "" : args.get("Destination").getValue().toString();
+            
+            int statusCode = controller.postFirewallRules(url, src, dst);
+
+            return new SubmodelElement[] {
+                new Property("StatusCode: " + statusCode)
+            };
+        };
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> getFirewallRules(String url) {
+        return (args) -> {
+            AccessControlList[] res = controller.getResponseWithSerial(url, new TypeReference<AccessControlList[]>(){});
+            return res[0].formatRules();
+        };
+    }
+
+    public Function<Map<String, SubmodelElement>, SubmodelElement[]> deleteFirewallRules(String url) {
+        return (args) -> {
+            int statusCode = controller.deleteFirewallRules(url, ((BigInteger) args.get("RuleId").getValue()).intValue());
+
+            return new SubmodelElement[] {
+                new Property("StatusCode: " + statusCode)
+            };
+        };
+    }
+}
